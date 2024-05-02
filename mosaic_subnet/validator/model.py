b0a950b7a25381916018fc39a7261b5c052cbff8
mos4ic <mosaicxorg@gmail.com>
2024-05-02 22:40:16 +0800

    init nsfw checker

--- mosaic_subnet/validator/model.py
+++ mosaic_subnet/validator/model.py
@@ -3,6 +3,7 @@ from io import BytesIO
 import torch
 from PIL import Image
 from transformers import CLIPProcessor, CLIPModel
+from transformers import pipeline
 from loguru import logger
 
 from communex.module.module import Module, endpoint
@@ -33,6 +34,25 @@ class CLIP(Module):
         return {"model": self.model_name}
 
 
+class NSFWChecker(Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.model_name = "Falconsai/nsfw_image_detection"
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.classifier = pipeline(
+            "image-classification",
+            model="Falconsai/nsfw_image_detection",
+            device=self.device,
+        )
+
+    def check_nsfw(self, file: bytes) -> bool:
+        image = Image.open(BytesIO(file))
+        for c in self.classifier(image):
+            if c["label"] == "nsfw" and c["score"] > 0.8:
+                return True
+        return False
+
+
 if __name__ == "__main__":
     import httpx
 
@@ -42,3 +62,7 @@ if __name__ == "__main__":
     score_cat = c.get_similarity(file=image, prompt="cat")
     score_dog = c.get_similarity(file=image, prompt="dog")
     print(score_cat, score_dog)
+
+    nc = NSFWChecker()
+    is_nsfw = nc.check_nsfw(image)
+    print(is_nsfw)
