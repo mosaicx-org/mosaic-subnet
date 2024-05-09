ac01438a6e7fae561f08fc419a2a66ad8bdbd223
Mosaic <48199614+mos4ic@users.noreply.github.com>
2024-05-10 02:44:44 +0800

    fix uid 0

--- mosaic_subnet/base/__init__.py
+++ mosaic_subnet/base/__init__.py
@@ -92,9 +92,7 @@ class BaseValidator:
 
         modules_filtered_address = get_ip_port(modules_addresses)
         for module_id in modules_keys.keys():
-            if module_id == 0:  # skip master
-                continue
-            if modules_keys[module_id] == val_ss58:  # skip yourself
+            if modules_keys[module_id] == val_ss58:
                 continue
             module_addr = modules_filtered_address.get(module_id, None)
             if not module_addr:
